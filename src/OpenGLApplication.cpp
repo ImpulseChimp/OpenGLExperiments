95bd1d4220b251af2d458f8ecbd00d4a8a4db609
Christopher M. Wood <cwood@qualtrax.com>
2016-06-28 03:02:55 -0400

    Working example from chapter 1

--- src/OpenGLApplication.cpp
+++ src/OpenGLApplication.cpp
@@ -1,63 +1,120 @@
 #define GLEW_STATIC
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
 #include <GL/glew.h>
 #include <GL/glut.h>
 #include <GLM/glm.hpp>
 
+#include "vgl.h"
+#include "LoadShaders.h"
+
+using namespace std;
 using namespace glm;
+
+void initializeWindow(int argc, char **argv, bool fullScreen);
+void changeSize(int w, int h);
+void renderScene(void);
+void initializeDependencies(void);
+
+enum VAO_IDs { Triangles, VAOSize };
+enum Buffer_IDs { ArrayBuffer, BufferSize };
+enum Attrib_IDs { vPosition = 0 };
+
+GLuint vertexArrays[VAOSize];
+GLuint buffers[BufferSize];
+const GLuint VerticesCount = 6;
+
+int main(int argc, char **argv) {
+
+	initializeWindow(argc, argv, false);
+	initializeDependencies();
+
+	glutMainLoop();
+	return 1;
+}
+
+void initializeWindow(int argc, char **argv, bool fullScreen) {
+	float aspectRatio = 16.0/9.0;
+	int windowHeight = 400;
+	int windowWidth = windowHeight * aspectRatio;
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA);
+	glutInitWindowPosition(300, 100);
+	glutInitWindowSize(windowWidth, windowHeight);
+	glClearColor(1, 0, 0, 1);
+	glutCreateWindow("Shark Week 2016");
+
+	if (glewInit()) {
+		cerr << "Unable to load GLEW" << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	if (fullScreen)
+		glutFullScreen();
+
+	glutInitContextVersion(4, 3);
+	glutDisplayFunc(renderScene);
+	//glutReshapeFunc(changeSize);
+}
+
+void initializeDependencies(void) {
+
+	glGenVertexArrays(VAOSize, vertexArrays);
+	glBindVertexArray(vertexArrays[Triangles]);
+
+	GLfloat vertices[VerticesCount][2] = {
+		{ -0.90, -0.90 },
+		{  0.85, -0.90 },
+		{ -0.90,  0.85 },
+		{  0.90, -0.85 },
+		{  0.90,  0.90 },
+		{ -0.85,  0.90 }
+	};
+
+	glGenBuffers(BufferSize, buffers);
+	glBindBuffer(GL_ARRAY_BUFFER, buffers[ArrayBuffer]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	ShaderInfo shaders[] = {
+		{ GL_VERTEX_SHADER, "C:\\Users\\Christopher\\workspace\\OpenGLApplication\\src\\shaders\\vertex\\triangles.vert" },
+		{ GL_FRAGMENT_SHADER, "C:\\Users\\Christopher\\workspace\\OpenGLApplication\\src\\shaders\\fragment\\triangles.frag" },
+		{ GL_NONE, NULL }
+	};
+
+	GLuint program = LoadShaders(shaders);
+	glUseProgram(program);
+
+	glVertexAttribPointer(vPosition, 2, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
+	glEnableVertexAttribArray(vPosition);
+
+	glClearColor(0, 1, 0, 1);
+}
 
 void changeSize(int w, int h) {
 
-	// Prevent a divide by zero, when window is too short
-	// (you cant make a window of zero width).
 	if (h == 0)
 		h = 1;
 
 	float ratio =  w * 1.0 / h;
 
-	// Use the Projection Matrix
 	glMatrixMode(GL_PROJECTION);
-
-	// Reset Matrix
 	glLoadIdentity();
-
-	// Set the viewport to be the entire window
 	glViewport(0, 0, w, h);
+	gluPerspective(45, ratio, 1, 100);
 
-	// Set the correct perspective.
-	gluPerspective(45,ratio,1,100);
-
-	// Get Back to the Modelview
 	glMatrixMode(GL_MODELVIEW);
 }
 
 void renderScene(void) {
 
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT);
 
-	glBegin(GL_TRIANGLES);
-		glVertex3f(-2,-2,-5.0);
-		glVertex3f(2,0.0,-5.0);
-		glVertex3f(0.0,2,-5.0);
-	glEnd();
+	glBindVertexArray(vertexArrays[Triangles]);
+	glDrawArrays(GL_TRIANGLES, 0, VerticesCount);
 
-	glutSwapBuffers();
+	glFlush();
 }
 
-int main(int argc, char **argv) {
-
-	// init GLUT and create window
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowPosition(100,100);
-	glutInitWindowSize(320,320);
-	glutCreateWindow("Lighthouse3D - GLUT Tutorial");
-
-	// register callbacks
-	glutDisplayFunc(renderScene);
-	glutReshapeFunc(changeSize);
-
-	// enter GLUT event processing loop
-	glutMainLoop();
-
-	return 1;
-}
