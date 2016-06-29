54dc23dec3725469a5a14c3d9b34a37715ce5fdd
Christopher M. Wood <cwood@qualtrax.com>
2016-06-28 21:34:56 -0400

    Initial time step game loop written

--- src/OpenGLApplication.cpp
+++ src/OpenGLApplication.cpp
@@ -17,29 +17,75 @@ void initializeWindow(int argc, char **argv, bool fullScreen);
 void changeSize(int w, int h);
 void renderScene(void);
 void initializeDependencies(void);
+void processKeyboardInput(unsigned char key, int x, int y);
+void processMouseInput(int button, int state, int x, int y);
+void toggleFullScreen();
+void processInput();
+void updateGameState();
+int getCurrentTime();
 
 enum VAO_IDs { Triangles, VAOSize };
 enum Buffer_IDs { ArrayBuffer, BufferSize };
-enum Attrib_IDs { vPosition = 0 };
+enum Attrib_IDs { vPosition = 0, vOffset = 6 };
+
+enum Game_Loop { MS_PER_UPDATE = 16 };
 
 GLuint vertexArrays[VAOSize];
 GLuint buffers[BufferSize];
 const GLuint VerticesCount = 6;
+bool isFullscreen = false;
+
+float aspectRatio = 16.0/9.0;
+int windowHeight = 400;
+int windowWidth = windowHeight * aspectRatio;
+
 
 int main(int argc, char **argv) {
 
 	initializeWindow(argc, argv, false);
 	initializeDependencies();
 
-	glutMainLoop();
+	bool runGame = true;
+	double previous = getCurrentTime();
+	double lag = 0.0;
+
+	while (runGame)
+	{
+		double current = getCurrentTime();
+		double elapsed = current - previous;
+		previous = current;
+		lag += elapsed;
+
+		//processInput();
+
+		while (lag >= MS_PER_UPDATE)
+		{
+			updateGameState();
+			lag -= MS_PER_UPDATE;
+		}
+
+		renderScene();
+	}
+
 	return 1;
 }
 
-void initializeWindow(int argc, char **argv, bool fullScreen) {
-	float aspectRatio = 16.0/9.0;
-	int windowHeight = 400;
-	int windowWidth = windowHeight * aspectRatio;
+int getCurrentTime()
+{
+	return glutGet(GLUT_ELAPSED_TIME);
+}
 
+void processInput()
+{
+
+}
+
+void updateGameState()
+{
+
+}
+
+void initializeWindow(int argc, char **argv, bool fullScreen) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA);
 	glutInitWindowPosition(300, 100);
@@ -57,6 +103,9 @@ void initializeWindow(int argc, char **argv, bool fullScreen) {
 
 	glutInitContextVersion(4, 3);
 	glutDisplayFunc(renderScene);
+	glutKeyboardFunc(processKeyboardInput);
+	glutMouseFunc(processMouseInput);
+	//glutIdleFunc(renderScene);
 	//glutReshapeFunc(changeSize);
 }
 
@@ -65,13 +114,14 @@ void initializeDependencies(void) {
 	glGenVertexArrays(VAOSize, vertexArrays);
 	glBindVertexArray(vertexArrays[Triangles]);
 
-	GLfloat vertices[VerticesCount][2] = {
+	GLfloat vertices[VerticesCount + 1][2] = {
 		{ -0.90, -0.90 },
 		{  0.85, -0.90 },
 		{ -0.90,  0.85 },
 		{  0.90, -0.85 },
 		{  0.90,  0.90 },
-		{ -0.85,  0.90 }
+		{ -0.85,  0.90 },
+		{  3.00,  3.00 }
 	};
 
 	glGenBuffers(BufferSize, buffers);
@@ -90,7 +140,12 @@ void initializeDependencies(void) {
 	glVertexAttribPointer(vPosition, 2, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
 	glEnableVertexAttribArray(vPosition);
 
+	glVertexAttribPointer(vOffset, 2, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(VerticesCount));
+	glEnableVertexAttribArray(vOffset);
+
 	glClearColor(0, 1, 0, 1);
+	//glPointSize(1.0);
+	glutMainLoop();
 }
 
 void changeSize(int w, int h) {
@@ -112,9 +167,49 @@ void renderScene(void) {
 
 	glClear(GL_COLOR_BUFFER_BIT);
 
+	glColor3f(1.0f, 1.0f, 0.0f);
 	glBindVertexArray(vertexArrays[Triangles]);
 	glDrawArrays(GL_TRIANGLES, 0, VerticesCount);
+	cerr << "RENDERED" << endl;
 
 	glFlush();
 }
 
+void processKeyboardInput(unsigned char key, int x, int y) {
+	switch(key) {
+	case 'w':
+
+		break;
+	case 'a':
+
+		break;
+	case 's':
+
+		break;
+	case 'd':
+
+		break;
+	case 'f':
+		toggleFullScreen();
+		break;
+	case 27:
+		exit(0);
+		break;
+	}
+}
+
+void processMouseInput(int button, int state, int x, int y) {
+
+}
+
+void toggleFullScreen() {
+	if (!isFullscreen)
+	{
+		glutFullScreen();
+		isFullscreen = true;
+	}
+	else {
+		glutReshapeWindow(windowWidth, windowHeight);
+		isFullscreen = false;
+	}
+}
--- src/shaders/vertex/triangles.vert
+++ src/shaders/vertex/triangles.vert
@@ -1,9 +1,12 @@
 #version 430
 
-in vec4 vPosition;
+layout (location = 0) in vec4 vPosition;
+layout (location = 6) in vec4 vOffset;
 
 void 
 main()
 {
+    vPosition[0] += offset[0];
+    vPosition[1] += offset[0];
 	gl_Position = vPosition;
 }
